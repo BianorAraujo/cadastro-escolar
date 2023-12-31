ca64e19796e7a12bb119f9277e1b3e12d62a1cb6
Barbara Machado <barbara.machado@Provecto.local>
2023-12-31 14:09:20 -0300

    Criacao do projeto backend

---
+++ backend/CadastroAPI/CadastroAPI/Controllers/UsuarioController.cs
@@ -0,0 +1,32 @@
+﻿using CadastroAPI.Models;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace CadastroAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsuarioController : Controller
+    {
+        private readonly string _connectionString;
+
+        public UsuarioController(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DbConnection");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            using (var sqlConnectin = new SqlConnection(_connectionString))
+            {
+                const string sql = "SELECT * FROM Usuarios";
+
+                var usuarios = await sqlConnectin.QueryAsync<UsuarioModel>(sql);
+
+                return Ok(usuarios);
+            }
+        }
+    }
+}
